0a9be36acc766062396a97134e4853684c5ef253
dishant purbia <dishantpurbia@email.com>
2026-06-02 14:07:34 +0530

    fix production cookies

--- backend/utils/util.js
+++ backend/utils/util.js
@@ -14,7 +14,7 @@ const genneratetoken = (userid, res) => {
     res.cookie("jwt", token, {
         maxAge: 7 * 24 * 60 * 60 * 1000,
         httpOnly: true,
-        sameSite:"strict",
+        sameSite:"none",
         secure : process.env.NODE_ENV === "development" ? false : true,
     });
 
